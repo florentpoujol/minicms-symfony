07b55e36e786e270d3733804c0eed5c9c0c9940a
Florent Poujol <florentpoujol@fastmail.com>
2024-09-23 15:20:08 +0200

    add route to show a single article

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
 use App\Repository\ArticleRepository;
+use Symfony\Bridge\Doctrine\Attribute\MapEntity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -19,6 +21,16 @@ final class BlogController extends AbstractController
     {
         return $this->render('blog/index.html.twig', [
             'articles' => $this->articleRepository->getAllPublished(),
+        ]);
+    }
+
+    #[Route('/blog/{slug}', name: 'app_article_show')]
+    public function showArticle(
+        #[MapEntity(mapping: ['slug' => 'slug'])] // note: from the documentation, this should work without the explicit mapping
+        Article $article
+    ): Response {
+        return $this->render('blog/article.html.twig', [
+            'article' => $article,
         ]);
     }
 }

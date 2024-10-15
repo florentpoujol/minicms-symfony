a2429a7061e41cd484dbb95457f1d1c87da1c184
Florent Poujol <florentpoujol@fastmail.com>
2024-10-15 17:07:36 +0200

    more work on the AuditLogListener, almost handling updates

--- src/EventListener/AuditLogListener.php
+++ src/EventListener/AuditLogListener.php
@@ -9,16 +9,18 @@ use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Event\PostPersistEventArgs;
 use Doctrine\ORM\Event\PostRemoveEventArgs;
-use Doctrine\ORM\Event\PostUpdateEventArgs;
+use Doctrine\ORM\Event\PreUpdateEventArgs;
 use Doctrine\ORM\Events;
 use Doctrine\Persistence\Event\LifecycleEventArgs;
 use Doctrine\Persistence\ObjectManager;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Http\Attribute\CurrentUser;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
 #[AsDoctrineListener(event: Events::postPersist, priority: 500, connection: 'default')]
-#[AsDoctrineListener(event: Events::postUpdate, priority: 500, connection: 'default')]
+#[AsDoctrineListener(event: Events::preUpdate, priority: 500, connection: 'default')]
 #[AsDoctrineListener(event: Events::postRemove, priority: 500, connection: 'default')]
 /**
  * TODO:
@@ -37,11 +39,11 @@ final class AuditLogListener
     ];
 
     public function __construct(
-        #[CurrentUser]
-        private readonly ?User                  $user,
-        private readonly ?Request               $request,
-        private readonly NormalizerInterface    $normalizer,
+        private readonly TokenStorageInterface $tokenStorage,
+        private readonly RequestStack $requestStack,
+        private readonly NormalizerInterface $normalizer,
         private readonly EntityManagerInterface $entityManager,
+        private readonly LoggerInterface $logger,
     )
     {
     }
@@ -51,7 +53,12 @@ final class AuditLogListener
         $this->saveLog($event, AuditLogAction::CREATE);
     }
 
-    public function postUpdate(PostUpdateEventArgs $event): void
+    /**
+     * Ideally here, we should use the postUpdate event so that we register the log only
+     * when we are sure the entity is updated, but the change set with the properties before and after arent't available
+     * in the post update event.
+     */
+    public function preUpdate(PreUpdateEventArgs $event): void
     {
         $this->saveLog($event, AuditLogAction::UPDATE);
     }
@@ -66,32 +73,36 @@ final class AuditLogListener
      */
     private function saveLog(LifecycleEventArgs $event, AuditLogAction $action): void
     {
+        static $i;
+        $i++;
         $entity = $event->getObject();
+        $this->logger->info($entity::class, ['i' => $i]); // doesn't work ?
+
         if (\in_array($entity::class, $this->ignoredEntities, true)) {
             return;
         }
 
         $log = new AuditLog();
         // $log->model()->associate($model);
-
         $log->setAction($action);
+        $log->setContext('{unknown}');
+        $log->setCreatedAt(new \DateTimeImmutable());
 
         if (\PHP_SAPI === 'cli') {
-            // find the name of the currently running Artisan command...
-            // In actual scenario, this is probably super slow and not worth it ?
-
-            $commandName = '{unknown}';
-
             // get the full actual CLI command entered in the terminal, which gives the options and arguments
             $args = implode(' ', $_SERVER['argv'] ?? []);
-
-            $log->setContext('cli: ' . $commandName . " ($args)");
+            $log->setContext("cli: ($args)");
 
             // could also get the name of the current job, if possible (probably need the same shenanigans as for the Artisan command name)
-        } else { // assume web
-            $log->setUser($this->user);
-            if ($this->request instanceof Request) {
-                $log->setContext('http:' . $this->request->getUri());
+        } else { // probably web
+            $user = $this->tokenStorage->getToken()->getUser(); // using the CurrentUser attribute didn't work...
+            if ($user instanceof User) {
+                $log->setUser($user);
+            }
+
+            $request = $this->requestStack->getCurrentRequest();
+            if ($request instanceof Request) {
+                $log->setContext('http:' . $request->getPathInfo() . '?' . $request->getQueryString());
             }
         }
 
@@ -101,20 +112,29 @@ final class AuditLogListener
         } elseif ($action === AuditLogAction::DELETE) {
             $data['before'] = $this->normalizer->normalize($entity, 'array');
         } elseif ($action === AuditLogAction::UPDATE) {
-            // keys are the changed attributes
-            // values are the current values
-            // $data['after'] = $model->getChanges();
-            //
-            // $data['before'] = [];
-            // foreach (array_keys($data['after']) as $changedAttribute) {
-            //     $data['before'][$changedAttribute] = $model->getOriginal($changedAttribute);
-            // }
+            $data['before'] = [];
+            $data['after'] = [];
+
+            assert($event instanceof PreUpdateEventArgs);
+            $changeSet = $event->getEntityChangeSet();
+
+            /**
+             * @var string $property
+             * @var array{0: mixed, 1: mixed} $beforeAndAfter 0 is before, 1 is after
+             */
+            foreach ($changeSet as $property => $beforeAndAfter) {
+                $data['before'][$property] = $beforeAndAfter[0];
+                $data['after'][$property] = $beforeAndAfter[1];
+            }
+
+            // FIXME florent: there is an infinite loop until out of memory
+            //  The audit_log table auto increment values show that this is due to a loop of AuditLog creation
+
         }
+        $log->setData($data); // @phpstan-ignore-line
 
         // remove sensitive properties ? (maybe done by the serializer)
-
-        $log->setData($data); // @phpstan-ignore-line
-        $log->setCreatedAt(new \DateTimeImmutable());
+        // ideally we would like to keep the sensitive properties, but redact their values
 
         $this->entityManager->persist($log);
         $this->entityManager->flush();
